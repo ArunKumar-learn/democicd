82480ddba85b31ad0d3a4174791c54fe74478c32
root <root@ip-172-31-32-253.ap-south-1.compute.internal>
2025-03-26 08:05:01 +0000

    "send commit"

--- application.py
+++ application.py
@@ -106,7 +106,8 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Congratulations</h1>
+    <h1>Congratulations Arun
+    </h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
     <p>This environment is launched with Elastic Beanstalk Python Platform</p>
   </div>
